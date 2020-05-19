a12ac3072fdc1e87b9234efbbdebd26e503171b1
Jimi <46426730+Bibjim@users.noreply.github.com>
2020-05-19 21:54:56 +0200

    First commit

---
+++ assets/js/animate.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#deontologie").click(function(){
+	   $('#show-deontologie').fadeIn(2000);
+    });
+});
+
+$(document).ready(function(){
+    $("#return").click(function(){
+	   $('#show-deontologie').fadeOut(2000);
+    });
+});
